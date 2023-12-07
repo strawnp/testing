98c5867282efe2898077040d29cb1d217d9111a3
Peter Strawn <peter.strawn@jesuithighschool.org>
2023-12-07 12:19:04 -0800

    Add math demo

--- Tester.java
+++ Tester.java
@@ -2,5 +2,9 @@ public class Tester {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Here is another line of code");
+
+        int a = 10;
+        int b = 7;
+        System.out.println(a + " + " + b + " = " + (a + b));
     }
 }
