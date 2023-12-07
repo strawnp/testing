33c5435d247c7d88d60230b4fdeb90a0ac38217b
Peter Strawn <peter.strawn@jesuithighschool.org>
2023-12-07 12:17:44 -0800

    Add a second line of text

--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Here is another line of code");
     }
 }
