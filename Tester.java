b4a7e4ec503ce0df292ca2866711ba88c52e86f5
Peter Strawn <peter.strawn@jesuithighschool.org>
2023-12-07 12:20:23 -0800

    Remove extra code

--- Tester.java
+++ Tester.java
@@ -1,10 +1,5 @@
 public class Tester {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Here is another line of code");
-
-        int a = 10;
-        int b = 7;
-        System.out.println(a + " + " + b + " = " + (a + b));
     }
 }
